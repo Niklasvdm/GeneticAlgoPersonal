087dc958585ff5baa7d5bb155725fc9d87b9f64a
Koenlaermans <36439934+Koenlaermans@users.noreply.github.com>
2022-10-18 15:06:42 +0200

    Update Population.py

--- Python Code/Population.py
+++ Python Code/Population.py
@@ -1,26 +1,42 @@
-import numpy
+import numpy as np
 from Agent import Agent
 import random
+from AgentUtilities import *
 
 
 class Population:
     size: int
-    agents: list[int]
+    age: int
+    cities: list[int]
+    graph: np.array
+    agents: list[Agent]
+    amount_of_permutations: int
+    probability_of_permutation: float
 
-    # Initiate Agent with age and list of cities in order of visitation.
-    #
+    # Initiate Population with size, graph and cities
     def __init__(self, population_size, graph, cities):
-        self.agents = [Agent(0, random.shuffle(cities)) for i in range(population_size)]
         self.size = population_size
+        self.age = 0
         self.cities = cities
+        self.graph = graph
+        self.agents = [Agent(0, random.sample(cities, len(cities))) for i in range(population_size)]
+        self.amount_of_permutations = len(cities) // 2
+        self.probability_of_permutation = 0.5
 
+    # Select top x agents with the highest fitness
+    def select(self):
+        self.agents.sort(key=lambda agent: evaluateAgent(agent, self.graph), reverse=True)
+        self.agents = self.agents[self.size // 3:]  # take top x agents (eg: population size/3)
 
-    def mutatePopulation(self):
+    # Mutate each agent
+    def mutate(self):
         for agent in self.agents:
-            agent.mutate()
+            agent.mutate(self.amount_of_permutations, self.probability_of_permutation)
 
-    def crossoverPopulation(self):
-        pass
+    # Crossover agents selected by top k tournament selection
+    def crossover(self):
+        self.topKTournamentSelection()
+        crossoverAgents( None, None, self.age)
 
-    def eliminatePopulation(self):
+    def topKTournamentSelection(self):
         pass
